fcd3827ebaa751f3c6e921db5ae7955b7b77e438
Alldbg <b.afellah.2018@gmail.com>
2022-05-23 22:25:08 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# daisyui-react-webpack-jwvgc8
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/daisyui-react-webpack-jwvgc8)
---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './app.css';
+
+const App = (props) => {
+  return (
+    <div>
+      <button> Hello World</button>
+    </div>
+  );
+};
+export default App;
